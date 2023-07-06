f55686a6cd7d49085d7b1c4e0b9251025a9f0a88
Pande <popovski.pande@gmail.com>
2023-07-07 00:39:45 +0200

    set up about me section

--- src/components/About.js
+++ src/components/About.js
@@ -1,89 +1,116 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
+import htmlIcon from '../icons/language-html5.svg';
+import cssIcon from '../icons/language-css3.svg';
+import jsIcon from '../icons/language-javascript.svg';
+import jestIcon from '../icons/jest.svg';
+import npmIcon from '../icons/npm.svg';
+import sassIcon from '../icons/sass.svg';
+import reactIcon from '../icons/react.svg';
+import webpackIcon from '../icons/webpack.svg';
+import gitIcon from '../icons/git.svg';
+import rightIcon from '../icons/chevron_right_FILL0_wght400_GRAD0_opsz48.svg';
+import leftIcon from '../icons/chevron_left_FILL0_wght400_GRAD0_opsz48.svg';
+import uniqid from 'uniqid';
+
 
 export default function About() {
   const skills = [
     {
-      index: 0,
-      name: 'HTML',
-      iconURL: ''
-    },
-    {
       index: 1,
-      name: 'CSS',
-      iconURL: ''
+      name: 'HTML',
+      iconURL: htmlIcon
     },
     {
       index: 2,
-      name: 'JAVASCRIPT',
-      iconURL: ''
+      name: 'CSS',
+      iconURL: cssIcon
     },
     {
       index: 3,
-      name: 'REACT',
-      iconURL: ''
+      name: 'JAVASCRIPT',
+      iconURL: jsIcon
     },
     {
       index: 4,
-      name: 'SASS',
-      iconURL: ''
+      name: 'REACT',
+      iconURL: reactIcon
     },
     {
       index: 5,
-      name: 'GIT',
-      iconURL: ''
+      name: 'SASS',
+      iconURL: sassIcon
     },
     {
       index: 6,
-      name: 'NPM',
-      iconURL: ''
+      name: 'GIT',
+      iconURL: gitIcon
     },
     {
       index: 7,
-      name: 'WEBPACK',
-      iconURL: ''
+      name: 'NPM',
+      iconURL: npmIcon
     },
     {
       index: 8,
+      name: 'WEBPACK',
+      iconURL: webpackIcon
+    },
+    {
+      index: 9,
       name: 'JEST',
-      iconURL: ''
+      iconURL: jestIcon
     },
   ]
 
   return(
     <section className="about">
-      <div className="left">
-        <h1 className="about-title">About Me</h1>
+      <div className="about-wrapper">
 
-        <p className="text">
-        Hello there! I'm Pande, an aspiring web developer who finds joy 
-        in transforming ideas into stunning digital realities. 
-        With a broad understanding of HTML, CSS, JavaScript, and more, 
-        I strive to create intuitive websites that captivate audiences 
-        and drive results.
-        </p>
+        <section className="top">
+          <h1 className="about-title">About Me</h1>
 
-        <p className="text">
-        My approach combines creativity, technical expertise, and a 
-        user-centric mindset. By utilizing the latest frameworks and employing 
-        best practices in web development, I ensure that every project I 
-        undertake is visually appealing, fully functional, and optimized for 
-        speed and performance.
-        </p>
+          <p className="text">
+          Hello there! I'm Pande, an aspiring web developer who finds joy 
+          in transforming ideas into stunning digital realities. 
+          With a broad understanding of HTML, CSS, JavaScript, and more, 
+          I strive to create intuitive websites that captivate audiences 
+          and drive results.
+          </p>
 
-        <p className="text">
-        Let's embark on this web development journey together and empower your 
-        online presence with an unforgettable digital experience!
-        </p>
-      </div>
+          <p className="text">
+          My approach combines creativity, technical expertise, and a 
+          user-centric mindset. By utilizing the latest frameworks and employing 
+          best practices in web development, I ensure that every project I 
+          undertake is visually appealing, fully functional, and optimized for 
+          speed and performance.
+          </p>
 
-      <div className="right">
-        <button className="prev" aria-label="previous"></button>
-        <div className="skills-carousel">
-          <div className="container">
+          <p className="text">
+          Let's embark on this web development journey together and empower your 
+          online presence with an unforgettable digital experience!
+          </p>
+        </section>
 
+        <section className="bottom" aria-label="list of skills">
+          <button className="navigate prev" aria-label="previous">
+            <img src={leftIcon} alt="left arrow" />
+          </button>
+          <div className="skills-carousel">
+            <div className="container">
+              {skills.map((skill) => {
+                return (
+                  <button className="skill" key={uniqid()}>
+                    <img src={skill.iconURL} alt={skill.name} className="icon" />
+                    <p className="text">{skill.index}. {skill.name}</p>
+                  </button>
+                )
+              })}
+            </div>
           </div>
-        </div>
-        <button className="next" aria-label="next"></button>
+          <button className="navigate next" aria-label="next">
+            <img src={rightIcon} alt="right arrow" />
+          </button>
+        </section>
       </div>
     </section>
   )
