fa692f1c574d551b112d4535aa8ae3fb2dad74ed
Pande <popovski.pande@gmail.com>
2023-07-08 16:12:22 +0200

    set up contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,30 @@
+import githubIcon from '../icons/github.svg';
+import linkediIcon from '../icons/linkedin.svg';
+
 export default function Contact() {
   return(
-    <div className="contact">
-      Hello from Contact
-    </div>
+    <section className="contact" aria-label="contact">
+      <div className="contact-wrapper">
+        <h1 className="contact-title">Contact</h1>
+
+        <a 
+          className="button" 
+          href='https://github.com/Duesenberg' 
+          target="_blank"
+          rel="noreferrer">
+            <img className="icon" src={githubIcon} alt="github" />
+            <p className="text">Github</p>
+        </a>
+
+        <a 
+          className="button" 
+          href='https://www.linkedin.com/in/pande-popovski-96b526230/' 
+          target="_blank"
+          rel="noreferrer">
+            <img className="icon" src={linkediIcon} alt="linked in" />
+            <p className="text">LinkedIn</p>
+        </a>
+      </div>
+    </section>
   )
 };
